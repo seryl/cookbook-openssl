87b61eb6024313da53adcc44395e02b2a58d61ae
Josh Toft <josh@ooyala.com>
2013-11-21 10:45:56 -0800

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
-maintainer       "Opscode, Inc."
-maintainer_email "cookbooks@opscode.com"
+maintainer       "Josh Toft"
+maintainer_email "joshtoft@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures openssl"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1"
 
 recipe "openssl", "Empty, this cookbook provides a library, see README"
